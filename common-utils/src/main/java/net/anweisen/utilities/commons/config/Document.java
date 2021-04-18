c3979f2529979b477ac0f9fefefe92dc15ed8d6b
anweisen <anweisen@web.de>
2021-04-18 13:01:25 +0200

    Added some comments for explaination

--- common-utils/src/main/java/net/anweisen/utilities/commons/config/Document.java
+++ common-utils/src/main/java/net/anweisen/utilities/commons/config/Document.java
@@ -1,5 +1,7 @@
 package net.anweisen.utilities.commons.config;
 
+import net.anweisen.utilities.commons.config.document.EmptyDocument;
+
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -14,9 +16,25 @@ import java.util.List;
  */
 public interface Document extends Config, Json {
 
+	/**
+	 * Gets the document located at the given path.
+	 * If there is no document assigned to this path,
+	 * a new document is being created and assigned to this path.
+	 *
+	 * @return the document assigned to this path
+	 */
 	@Nonnull
 	Document getDocument(@Nonnull String path);
 
+	/**
+	 * Returns the list of documents located at the given path.
+	 * The returned list will not contain any null elements.
+	 * If there are no documents, the list will be empty.
+	 * Elements which are no documents in the original list will be skipped.
+	 * If a element is {@code null} an empty document will be added.
+	 *
+	 * @return the list of documents assigned to this path
+	 */
 	@Nonnull
 	List<Document> getDocumentList(@Nonnull String path);
 
@@ -29,9 +47,21 @@ public interface Document extends Config, Json {
 	@Nonnull
 	<T> T getSerializable(@Nonnull String path, @Nonnull T def);
 
+	/**
+	 * Returns the parent document of this document.
+	 * If {@code this} is the {@link #getRoot() root} the this method will return {@code null}.
+	 *
+	 * @return the parent document of this document
+	 */
 	@Nullable
 	Document getParent();
 
+	/**
+	 * Returns the root document of this document.
+	 * If the {@link #getParent() parent} is {@code null} this will return {@code this}.
+	 *
+	 * @return the root document of this document
+	 */
 	@Nonnull
 	Document getRoot();
 
