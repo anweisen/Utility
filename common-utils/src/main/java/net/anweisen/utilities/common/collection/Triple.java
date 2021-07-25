d6bc42be7dc18810306af95d7d53bd754f2a226f
anweisen <anweisen@web.de>
2021-07-25 19:56:28 +0200

    Add pairs

--- common-utils/src/main/java/net/anweisen/utilities/common/collection/Triple.java
+++ common-utils/src/main/java/net/anweisen/utilities/common/collection/Triple.java
@@ -1,83 +1,23 @@
 package net.anweisen.utilities.common.collection;
 
 import javax.annotation.Nullable;
-import java.util.Objects;
 
 /**
  * @author anweisen | https://github.com/anweisen
  * @since 1.1
+ *
+ * @param <F> The type of the first value
+ * @param <S> The type of the second value
+ * @param <T> The type of the third value
  */
-public class Triple<F, S, T> {
-
-	protected F first;
-	protected S second;
-	protected T third;
+@Deprecated
+public class Triple<F, S, T> extends net.anweisen.utilities.common.collection.pair.Triple<F, S, T> {
 
 	public Triple() {
 	}
 
 	public Triple(@Nullable F first, @Nullable S second, @Nullable T third) {
-		this.first = first;
-		this.second = second;
-		this.third = third;
-	}
-
-	public F getFirst() {
-		return first;
-	}
-
-	public S getSecond() {
-		return second;
-	}
-
-	public T getThird() {
-		return third;
-	}
-
-	public void setFirst(@Nullable F first) {
-		this.first = first;
-	}
-
-	public void setSecond(@Nullable S second) {
-		this.second = second;
-	}
-
-	public void setThird(@Nullable T third) {
-		this.third = third;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Triple<?, ?, ?> triple = (Triple<?, ?, ?>) o;
-		return Objects.equals(first, triple.first) && Objects.equals(second, triple.second) && Objects.equals(third, triple.third);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(first, second, third);
-	}
-
-	@Override
-	public String toString() {
-		return "Triple[" + first + ", " + second + ", " + third + "]";
-	}
-
-	public static <F, S, T> Triple<F, S, T> ofFirst(@Nullable F first) {
-		return new Triple<>(first, null, null);
-	}
-
-	public static <F, S, T> Triple<F, S, T> ofSecond(@Nullable S second) {
-		return new Triple<>(null, second, null);
-	}
-
-	public static <F, S, T> Triple<F, S, T> ofThird(@Nullable T third) {
-		return new Triple<>(null, null, third);
-	}
-
-	public static <F, S, T> Triple<F, S, T> of(@Nullable F first, @Nullable S second, @Nullable T third) {
-		return new Triple<>(first, second, third);
+		super(first, second, third);
 	}
 
 }
---
+++ common-utils/src/main/java/net/anweisen/utilities/common/collection/pair/Triple.java
@@ -0,0 +1,115 @@
+package net.anweisen.utilities.common.collection.pair;
+
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Objects;
+import java.util.function.Function;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 1.1
+ *
+ * @param <F> The type of the first value
+ * @param <S> The type of the second value
+ * @param <T> The type of the third value
+ */
+public class Triple<F, S, T> {
+
+	protected F first;
+	protected S second;
+	protected T third;
+
+	public Triple() {
+	}
+
+	public Triple(@Nullable F first, @Nullable S second, @Nullable T third) {
+		this.first = first;
+		this.second = second;
+		this.third = third;
+	}
+
+	public F getFirst() {
+		return first;
+	}
+
+	public S getSecond() {
+		return second;
+	}
+
+	public T getThird() {
+		return third;
+	}
+
+	public void setFirst(@Nullable F first) {
+		this.first = first;
+	}
+
+	public void setSecond(@Nullable S second) {
+		this.second = second;
+	}
+
+	public void setThird(@Nullable T third) {
+		this.third = third;
+	}
+
+	@Nonnull
+	@CheckReturnValue
+	public <ToF, ToS, ToT> Triple<ToF, ToS, ToT> map(@Nonnull Function<? super F, ? extends ToF> firstMapper,
+	                                                 @Nonnull Function<? super S, ? extends ToS> secondMapper,
+	                                                 @Nonnull Function<? super T, ? extends ToT> thirdMapper) {
+		return of(firstMapper.apply(first), secondMapper.apply(second), thirdMapper.apply(third));
+	}
+
+	public boolean noneNull() {
+		return first != null && second != null && third != null;
+	}
+
+	public boolean allNull() {
+		return first == null && second == null && third == null;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Triple<?, ?, ?> triple = (Triple<?, ?, ?>) o;
+		return Objects.equals(first, triple.first) && Objects.equals(second, triple.second) && Objects.equals(third, triple.third);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(first, second, third);
+	}
+
+	@Override
+	public String toString() {
+		return "Triple[" + first + ", " + second + ", " + third + "]";
+	}
+
+	@Nonnull
+	public static <F, S, T> Triple<F, S, T> ofFirst(@Nullable F first) {
+		return of(first, null, null);
+	}
+
+	@Nonnull
+	public static <F, S, T> Triple<F, S, T> ofSecond(@Nullable S second) {
+		return of(null, second, null);
+	}
+
+	@Nonnull
+	public static <F, S, T> Triple<F, S, T> ofThird(@Nullable T third) {
+		return of(null, null, third);
+	}
+
+	@Nonnull
+	public static <F, S, T> Triple<F, S, T> of(@Nullable F first, @Nullable S second, @Nullable T third) {
+		return new Triple<>(first, second, third);
+	}
+
+	@Nonnull
+	public static <F, S, T> Triple<F, S, T> empty() {
+		return new Triple<>();
+	}
+
+}
